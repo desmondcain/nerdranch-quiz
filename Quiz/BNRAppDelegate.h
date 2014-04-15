6aa83c101ae8d349e091cf5f08a00fd2c6170f7e
desmondcain <desmondcain@gmail.com>
2014-04-14 22:19:59 -0500

    Initial Commit

---
+++ Quiz/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Quiz
+//
+//  Created by Desmond on 4/14/14.
+//  Copyright (c) 2014 Desmond Cain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
