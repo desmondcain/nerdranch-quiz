b9464700263e7862da390bde28d6d556c0293501
desmondcain <desmondcain@gmail.com>
2014-04-16 23:58:15 -0500

    Declared queston and answer label outlets for UILabel.

--- Quiz/BNRQuizViewController.m
+++ Quiz/BNRQuizViewController.m
@@ -10,29 +10,13 @@
 
 @interface BNRQuizViewController ()
 
+@property (nonatomic, weak) IBOutlet UILabel *questionLabel;
+@property (nonatomic, weak) IBOutlet UILabel *answerLabel;
+
 @end
 
 @implementation BNRQuizViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
