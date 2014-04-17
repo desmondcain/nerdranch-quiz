3d0d31f7dddfda775b3fc3da372488d73401e235
desmondcain <desmondcain@gmail.com>
2014-04-17 01:21:16 -0500

    Implemented complete showQuestion & showAnswer action methods.

--- Quiz/BNRQuizViewController.m
+++ Quiz/BNRQuizViewController.m
@@ -27,7 +27,7 @@
     // Call the init method implemented by the superclass
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     
-    // Creat two arrays filled with questions and answers
+    // Create two arrays filled with questions and answers
     // and make the pointers point to them
     if (self) {
         self.questions = @[@"From what is cognac made?",
@@ -43,5 +43,35 @@
     return self;
 }
 
+- (IBAction)showQuestion:(id)sender
+{
+    // Step to the next question
+    self.currentQuestionIndex++;
+    
+    // Am I past the last question?
+    if (self.currentQuestionIndex == [self.questions count])
+    {
+        // Go back to the first question
+        self.currentQuestionIndex = 0;
+    }
+    
+    // Get the string at that index in the questions array
+    NSString *question = self.questions[self.currentQuestionIndex];
+    
+    // Display the string in the question label
+    self.questionLabel.text = question;
+    
+    // Reset the answer label
+    self.answerLabel.text = @"???";
+}
+
+- (IBAction)showAnswer:(id)sender
+{
+    // What is the answer to the current question?
+    NSString *answer = self.answers[self.currentQuestionIndex];
+    
+    // Display it in the answer label
+    self.answerLabel.text = answer;
+}
 
 @end
